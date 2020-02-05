806c3f32a9069ddb645c34d3039f34280f6dca3d
jchenriquez <jc.henriquez04@gmail.com>
2020-02-05 07:59:14 -0500

    working solution for all cases. space efficiency has to be improved.

--- decodeways.go
+++ decodeways.go
@@ -1,30 +1,59 @@
 package decodeways
 
-import "strconv"
-
-func decoder (s string, memo map[string] bool) int {
-  
-  
-
-  if count, saw := memo[s]; saw {
-    return count
-  }
-
-  var count int
-
-  for i := 0; i < len(s); i++ {
-    count += decoder(s[:i+1], memo)
-    count += decoder(s[i+1:], memo)
-  }
-
-  memo[s] = count
-  return count
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 // NumDecodings will return the amount of ways s can be decoded.
 func NumDecodings(s string) (ret int) {
-  if len(s) == 0 {
-    return 0
-  }
-	return decoder(s, make(map[string] bool))
+	if len(s) == 0 {
+		return 0
+	}
+
+	numWays := make([][]byte, 0, len(s))
+
+	for i := 0; i < len(s); i++ {
+		inDigit := s[i] - '0'
+
+		if len(numWays) == 0 {
+			if inDigit == 0 {
+				return 0
+			}
+			numWays = append(numWays, []byte{s[i] - '0'})
+		} else {
+			tmpNumWays := make([][]byte, 0, len(numWays)+1)
+			for _, bytArr := range numWays {
+				comb := fmt.Sprintf("%d%d", bytArr[len(bytArr)-1], inDigit)
+				fmt.Printf("comb %s\n", comb)
+				combInt, _ := strconv.Atoi(comb)
+				fmt.Printf("inDigit1 %d\n", inDigit)
+				fmt.Printf("combInt %d\n", combInt)
+
+				if combInt <= 26 {
+					bytArrCpy := make([]byte, len(bytArr))
+					copy(bytArrCpy, bytArr[:len(bytArr)-1])
+					bytArrCpy = append(bytArrCpy, byte(combInt))
+					tmpNumWays = append(tmpNumWays, bytArrCpy)
+				} else {
+					if inDigit == 0 {
+						continue
+					}
+				}
+
+				if inDigit != 0 {
+					tmpNumWays = append(tmpNumWays, append(bytArr, inDigit))
+				}
+			}
+
+			if len(tmpNumWays) == 0 {
+				return 0
+			}
+
+			numWays = tmpNumWays
+		}
+
+	}
+
+	return len(numWays)
 }
